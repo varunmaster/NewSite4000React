b7e4b9d70a683a9eb2bf96f2a7b8a0763cd4460d
Varun Master <varunmaster95@gmail.com>
2021-02-07 22:21:38 -0500

    Got modal to work and display the correct info. TODO prettyfy it and make it nice

---
+++ client/src/components/MovieModal.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+
+const MovieModal = ({ movieObj }) => {
+  const [modal, setModal] = useState(false);
+  const toggle = () => setModal(!modal);
+
+  return (
+    <>
+    {console.log("look here dummy:\n", movieObj)}
+      {/* <Modal isOpen={modal} toggle={toggle}>
+        <ModalHeader toggle={toggle}>{movieObj.Name}</ModalHeader>
+        <ModalBody>
+          {movieObj.Plot}
+        </ModalBody> */}
+
+
+        {/* this will be links to imDB website
+        <ModalFooter>
+          <Button color="primary" onClick={toggle}>Do Something</Button>{' '}
+        </ModalFooter> 
+      </Modal>*/}
+    </>
+  );
+};
+
+export default MovieModal;
