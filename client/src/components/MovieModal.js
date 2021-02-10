880a173c80c3e35a5234d56f9e9089ee6f51d804
Varun Master <varunmaster95@gmail.com>
2021-02-09 20:33:57 -0500

    done prettyfying the listMovies page and the modal. TODO HomePage and ListMovies page

--- client/src/components/MovieModal.js
+++ client/src/components/MovieModal.js
@@ -4,50 +4,75 @@ import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 const MovieModal = ({ selectedMovie, open, close, movieObj }) => {
   //const [modal, setModal] = useState(false);
   //const toggle = () => setModal(!modal);
-  const { Title, Plot, Year, Runtime, Released, Poster, imdbID } = selectedMovie;
+  const { Title, Plot, Year, Runtime, Released, Poster, imdbID, Actors, Ratings } = selectedMovie;
   const fullName = Title + " (" + Year + ")";
   const link = "https://www.imdb.com/title/" + imdbID;
 
   return (
     <>
-      {console.log("look here dummy:\n", selectedMovie, "\nopen is: ", open)}
-      {<Modal isOpen={open} toggle={close}>
-        <ModalHeader toggle={close}>
+      {/* {console.log("look here dummy:\n", selectedMovie, "\nopen is: ", open)} */}
+      {<Modal isOpen={open} toggle={close} className="modal-dialog modal-dialog-centered modal-lg">
+        <ModalHeader toggle={close} className="bg-dark">
           <h2><b>{fullName}</b></h2>
           <small>Date added to Plex: {movieObj[fullName]}</small>
         </ModalHeader>
-        <ModalBody>
-          <div className="card bg-light mb-3">
-            <div className="card-body">
-              <h5 className="card-title">Plot:</h5>
-              <p className="card-text">{Plot}</p>
+        <ModalBody className="bg-dark">
+          <div className="row">
+            <div className="col-sm-3">
+              <img src={Poster} alt='Poster' width="250px" />
             </div>
-          </div>
-          <hr />
-          <div className="card bg-light mb-3">
-            <div className="card-body">
-              <h5 className="card-title">Runtime:</h5>
-              <p className="card-text">{Runtime}</p>
+            <div className='col-sm-1'></div>
+            <div className="col-sm-4">
+              <div className="card border-light bg-dark mb-3">
+                <div className="card-body">
+                  <h5 className="card-title">Plot:</h5>
+                  <p className="card-text">{Plot}</p>
+                </div>
+              </div>
+              <hr />
+              <div className="card border-light bg-dark mb-3">
+                <div className="card-body">
+                  <h5 className="card-title">Runtime:</h5>
+                  <p className="card-text">{Runtime}</p>
+                </div>
+              </div>
+              <hr />
+              <div className="card border-light bg-dark mb-3">
+                <div className="card-body">
+                  <h5 className="card-title">Released:</h5>
+                  <p className="card-text">{Released}</p>
+                </div>
+              </div>
             </div>
-          </div>
-          <hr />
-          <div className="card bg-light mb-3">
-            <div className="card-body">
-              <h5 className="card-title">Released:</h5>
-              <p className="card-text">{Released}</p>
+            <div className="col-sm-4">
+              <div>
+              <div className="card border-light bg-dark mb-3">
+                <div className="card-body">
+                  <h5 className="card-title">Ratings:</h5>
+                  <ul>{Ratings.map(rating => {
+                    return <li key={Ratings.indexOf(rating)}>{rating.Source}: {rating.Value}</li>
+                  })}</ul>
+                </div>
+              </div>
+              <hr />
+              <div className="card border-light bg-dark mb-3">
+                <div className="card-body">
+                  <h5 className="card-title">Cast:</h5>
+                  <ul>{Actors.split(",").map(actor => {
+                    return <li key={actor}>{actor}</li> 
+                  })}</ul>
+                </div>
+              </div>
+              </div>
             </div>
-          </div>
-          <hr />
-          <div className="card bg-light mb-3">
-            <div className="card-body">
-              <p className="card-text text-center"><img src={Poster} alt='Poster'/></p>
-            </div>
+
+            {/* <hr /> */}
           </div>
         </ModalBody>
-        <ModalFooter>
+        <ModalFooter className="bg-dark">
           <Button color="info" href={link} target='_blank'>Open IMDB</Button>{' '}
           <Button color="danger" onClick={close}>Close</Button>{' '}
-        </ModalFooter> }
+        </ModalFooter> 
       </Modal>}
     </>
   );
