66348799d918cf2c5f2a9fa4a587c75ffdeee63f
Varun Master <varunmaster95@gmail.com>
2021-02-10 20:03:20 -0500

    added as much as possible to HomePage. still sucks but its done tho

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { Card, Button, CardDeck, CardBody } from 'reactstrap';
+import movies from '../assets/movies.jpg';
+import shows from '../assets/shows.webp';
 
 class Home extends Component {
     state = {}
@@ -10,7 +13,7 @@ class Home extends Component {
         });
     };
 
-    componentDidMount() {}
+    componentDidMount() { }
 
     handleInputChange = event => { //this is boilerplate stuff and can be reused 
         const { name, value } = event.target;
@@ -22,7 +25,25 @@ class Home extends Component {
     render() {
         return (
             <>
-                <h1>Home Page</h1>
+                <div className="bg-dark text-center">
+                    <h1>Home Page</h1>
+                </div>
+                <div className="bg-dark text-center">
+                    <CardDeck className="bg-dark">
+                        <Card className="bg-dark text-center">
+                            <center><img width="350px" src={movies} alt="movies" /></center>
+                            <CardBody>
+                                <Button className="btn btn-lg btn-info" href="/listMovies">Show me the damn movies!</Button>
+                            </CardBody>
+                        </Card>
+                        <Card className="bg-dark text-center">
+                            <center><img width="300px" src={shows} alt="movies" /></center>
+                            <CardBody>
+                                {/* <Button className="btn btn-lg btn-info" href="/listShows">Show me the damn shows!</Button> */}
+                            </CardBody>
+                        </Card>
+                    </CardDeck>
+                </div>
             </>
         )
     }
